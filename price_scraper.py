e0c668d6e93da7c6aed80dfed2b6b46c50dfbfe5
Jonas Gebele <ge69cas@mytum.de>
2022-12-19 13:25:19 +0100

    Restructure Code

--- price_scraper.py
+++ price_scraper.py
@@ -2,11 +2,24 @@ import csv
 import time
 import requests
 
-def get_avg_spot_price_from_binance_api():
-    # Define Pair ALGO/USDT
-    pair = "ALGOUSDT"
-    
-    # Make a GET request to the Binance API
+HUMBLESWAP_ALGOUSDC = "SVZS7Q7QMVHZONDHZJHR4564VTMEX3OQ5DSYBWKR5FJFTPZLVG3EZIWC34"
+HUMBLESWAP_ALGOgoUSD = "LMU5MRQWB3DDSM7J3YY32OBURDL3FHEHQW7J6USTIR5J3HSCNSCDTGDTCU"
+PACT_ALGOUSDC = "L747MOJV43QCLY4HSWVPL2A5SW62IBLA5XPTI5R4HO32WVAWOO5OBCEP3A"
+PACT_ALGOUSDT = "KIQDDU4KRXFMUBMLQ75VN5R6RVQHNGHCEX43VSPYHGHBNW27AFNGEZ7EY4"
+TINYMAN_ALGOUSDC = "FPOU46NBKTWUZCNMNQNXRWNW3SMPOOK4ZJIN5WSILCWP662ANJLTXVRUKA"
+TINYMAN_ALGOUSDT = "54UTVEAHWMBYB4L4BNLAEJFWBUTLLERTULROEEP7774OK6FUTX4U5NX6RM"
+
+def get_avg_spot_price_from_binance_spot(pair: str):
+    """
+    Get the average spot price from Binance Spot for a given pair.
+
+    Parameters:
+    pair (str): The pair for which to get the average spot price.
+
+    Returns:
+    float: The average spot price for the given pair.
+    """
+    # Make a GET request to the Binance API to the given pair
     response = requests.get(f"https://api.binance.com/api/v3/ticker/bookTicker?symbol={pair}")
 
     # Extract the bidPrice and askPrice values from the API response to calculate the average orderbook price
@@ -18,7 +31,16 @@ def get_avg_spot_price_from_binance_api():
     spot_price = round(avg_price, 6)
     return spot_price
 
-def get_swap_price_from_address(address):
+def get_swap_price_from_address(address: str):
+    """
+    Get the swap price for a given address.
+
+    Parameters:
+    address (str): The address for which to get the swap price.
+
+    Returns:
+    float: The swap price for the given address.
+    """
     # Make a GET request to the Address of Pool Account we want to monitor
     response = requests.get(f"https://node.algoexplorerapi.io/v2/accounts/{address}")
 
@@ -30,13 +52,6 @@ def get_swap_price_from_address(address):
     swap_price = round(assets[0]["amount"] / amount, 6)
     return swap_price
 
-HUMBLESWAP_ALGOUSDC = "SVZS7Q7QMVHZONDHZJHR4564VTMEX3OQ5DSYBWKR5FJFTPZLVG3EZIWC34"
-HUMBLESWAP_ALGOgoUSD = "LMU5MRQWB3DDSM7J3YY32OBURDL3FHEHQW7J6USTIR5J3HSCNSCDTGDTCU"
-PACT_ALGOUSDC = "L747MOJV43QCLY4HSWVPL2A5SW62IBLA5XPTI5R4HO32WVAWOO5OBCEP3A"
-PACT_ALGOUSDT = "KIQDDU4KRXFMUBMLQ75VN5R6RVQHNGHCEX43VSPYHGHBNW27AFNGEZ7EY4"
-TINYMAN_ALGOUSDC = "FPOU46NBKTWUZCNMNQNXRWNW3SMPOOK4ZJIN5WSILCWP662ANJLTXVRUKA"
-TINYMAN_ALGOUSDT = "54UTVEAHWMBYB4L4BNLAEJFWBUTLLERTULROEEP7774OK6FUTX4U5NX6RM"
-
 def main():
     # Open the CSV file for writing
     with open("prices2.csv", "w", newline="") as f:
@@ -45,7 +60,8 @@ def main():
 
         # Write the header row
         writer.writerow([
-            "timestamp", "priceBinanceSpot[ALGOUSDT]",
+            "timestamp",
+            "priceBinanceSpot[ALGOUSDT]",
             "priceHumbeSwap[ALGOUSDC]",
             "priceHumbeSwap[ALGOgoUSD]",
             "pricePact[ALGOUSDC]",
@@ -55,11 +71,11 @@ def main():
         ])
 
     while True:
-        # Get the current timestamp
+        # Get the current timestamp in miliseconds
         timestamp = round(time.time(), 2)
 
         # Get the average price from the API
-        binance_usdt = get_avg_spot_price_from_binance_api()
+        binance_usdt = get_avg_spot_price_from_binance_spot("ALGOUSDT")
         humbleswap_usdc = get_swap_price_from_address(HUMBLESWAP_ALGOUSDC)
         humbleswap_gousd = get_swap_price_from_address(HUMBLESWAP_ALGOgoUSD)
         pact_usdc = get_swap_price_from_address(PACT_ALGOUSDC)
