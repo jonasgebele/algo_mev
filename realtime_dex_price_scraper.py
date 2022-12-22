5c7eeb95a0f0e2ea55054404b314e4d1e3366869
Jonas Gebele <ge69cas@mytum.de>
2022-12-22 20:01:53 +0100

    Update realtime_dex_price_scraper.py

--- realtime_dex_price_scraper.py
+++ realtime_dex_price_scraper.py
@@ -1,6 +1,7 @@
 import csv
 import time
 import requests
+import threading
 
 HUMBLESWAP_ALGOUSDC = "SVZS7Q7QMVHZONDHZJHR4564VTMEX3OQ5DSYBWKR5FJFTPZLVG3EZIWC34"
 HUMBLESWAP_ALGOgoUSD = "LMU5MRQWB3DDSM7J3YY32OBURDL3FHEHQW7J6USTIR5J3HSCNSCDTGDTCU"
@@ -10,15 +11,6 @@ TINYMAN_ALGOUSDC = "FPOU46NBKTWUZCNMNQNXRWNW3SMPOOK4ZJIN5WSILCWP662ANJLTXVRUKA"
 TINYMAN_ALGOUSDT = "54UTVEAHWMBYB4L4BNLAEJFWBUTLLERTULROEEP7774OK6FUTX4U5NX6RM"
 
 def get_avg_spot_price_from_binance_spot(pair: str):
-    """
-    Get the average spot price from Binance Spot for a given pair.
-
-    Parameters:
-    pair (str): The pair for which to get the average spot price.
-
-    Returns:
-    float: The average spot price for the given pair.
-    """
     # Default value for price to return in case of a exception
     spot_price = 0.000000
 
@@ -43,25 +35,21 @@ def get_avg_spot_price_from_binance_spot(pair: str):
         return spot_price
 
 def get_swap_price_from_address(address: str):
-    """
-    Get the swap price for a given address.
-
-    Parameters:
-    address (str): The address for which to get the swap price.
-
-    Returns:
-    float: The swap price for the given address.
-    """
     # Default value for price to return in case of a exception
-    spot_price = 0.000000
+    api_call_data = {
+        "swap_price": 0.000000,
+        "round": 0,
+        "pool_size": 0.000000
+    }
 
     try:
         # Make a GET request to the Address of Pool Account we want to monitor
-        response = requests.get(f"https://node.algoexplorerapi.io/v2/accounts/{address}")
+        response = requests.get(f"https://node.algoexplorerapi.io/v2/accounts/{address}").json()
 
         # Extract the amount and assets values from the API response
-        amount = response.json()['amount']
-        assets = response.json()['assets']
+        amount = response['amount']
+        assets = response['assets']
+        round = response['round']
 
         # Calculate the swap price as the assets[0]["amount"] divided by amount
         swap_price = round(assets[0]["amount"] / amount, 6)
@@ -75,10 +63,6 @@ def get_swap_price_from_address(address: str):
         return swap_price
 
 def main():
-    """
-    Get the spot prices from various sources and write them to a CSV file.
-    The prices are obtained continuously in a loop and written to the CSV file with a timestamp.
-    """
     # Open the CSV file for writing
     with open("prices2.csv", "w", newline="") as f:
         # Create a CSV writer
@@ -99,6 +83,24 @@ def main():
     while True:
         # Get the current timestamp in miliseconds
         timestamp = round(time.time(), 2)
+        '''
+        # Create a thread for each API call
+        api_1_thread = threading.Thread(target=get_avg_spot_price_from_binance_spot, args=("ALGOUSDT",))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(HUMBLESWAP_ALGOUSDC,))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(HUMBLESWAP_ALGOgoUSD,))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(PACT_ALGOUSDC,))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(PACT_ALGOUSDT,))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(TINYMAN_ALGOUSDC,))
+        api_2_thread = threading.Thread(target=get_swap_price_from_address, args=(TINYMAN_ALGOUSDT,))
+
+        # Start the threads
+        api_1_thread.start()
+        api_2_thread.start()
+
+        # Wait for the threads to finish
+        api_1_thread.join()
+        api_2_thread.join()
+        '''
 
         # Get the average price from the API
         binance_usdt = get_avg_spot_price_from_binance_spot("ALGOUSDT")
