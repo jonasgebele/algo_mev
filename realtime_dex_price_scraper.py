04328b2d6db210bc3ef59a00ed6d566b6660d768
Jonas Gebele <ge69cas@mytum.de>
2023-01-16 16:13:04 +0100

    Update realtime_dex_price_scraper.py

--- realtime_dex_price_scraper.py
+++ realtime_dex_price_scraper.py
@@ -52,58 +52,87 @@ def get_avg_spot_price_from_binance_spot(pair):
         return "Binance_ALGOUSDT", timestamp, spot_price, 0, 0
 
 def get_swap_price_from_address_at_range(markets, key):
-    # Make a GET request to the Address of Pool Account we want to monitor
-    response = requests.get(f"https://node.algoexplorerapi.io/v2/accounts/{markets[key]}").json()
-    # Extract the pool-sizes and asset values from the API response
-    assets = response['assets']
-    X = response['amount']
-    Y = assets[0]["amount"]
-    round = response['round']
-    # Calculate the swap price as the Y divided by X
-    swap_price = Y / X
-    return key, round, swap_price, X, Y
+    try:
+        # Make a GET request to the Address of Pool Account we want to monitor
+
+        # ------------------------------------------------------------------------------------------#
+        # ALGOEXPLORER INDEXER
+        #response = requests.get(f"https://node.algoexplorerapi.io/v2/accounts/{markets[key]}").json()
+
+        # TUM INDEXER
+        response = requests.get(f"http://131.159.14.109:8981/v2/accounts/{markets[key]}").json()
+        response = response["account"]
+        #-------------------------------------------------------------------------------------------#
+
+        # Extract the pool-sizes and asset values from the API response
+        assets = response['assets']
+        X = response['amount']
+        Y = assets[0]["amount"]
+        round = response['round']
+        
+        # Calculate the swap price as the Y divided by X
+        swap_price = Y / X
+    except requests.exceptions.RequestException as e:
+        # Handle any exceptions that occurred while making the API call
+        print(f"An error occurred: {e}")
+    except ValueError as e:
+        # Handle any value errors that occurred while processing the response
+        print(f"An error occurred: {e}")
+    except ZeroDivisionError:
+        print("Cannot divide by zero.")
+    finally:
+        return key, round, swap_price, X, Y
 
 def main():
+    # create dict with market-descriptors as keys and addresses of asset-pools as values
     markets = set_up_markets_dict()
+    # get consistent order of market-keys from the markets-dict
     ordered_list_of_market_keys = get_sorted_list_of_keys(markets)
     
     # Open the CSV file for writing
     with open("prices2.csv", "w", newline="") as f:
         # Create a CSV writer
         writer = csv.writer(f)
-
-        # Write the header row
+        # Write the header row with market-keys in consistent order
         writer.writerow([x for market_key in ordered_list_of_market_keys for x in ("range", market_key, "pool_size_X", "pool_size_Y")])
     
-    last_prices = None
+    # Store track of API response information from the latest call
+    latest_scraped_info = None
     while True:
-        prices = {}
+        # Dictionary for API response information for the current call
+        scraped_info = {}
+        # Keep track of rounds for each DEX info scraped
         rounds = set()
 
+        # Making API calls parallized instead of sequentially in order to reduce I/O waiting time
         with ThreadPoolExecutor() as executor:
             results = [executor.submit(get_swap_price_from_address_at_range, markets, market_key) for market_key in ordered_list_of_market_keys]
             # results.append(executor.submit(get_avg_spot_price_from_binance_spot, "ALGOUSDT"))
             for f in as_completed(results):
                 key, round, swap_price, X, Y = f.result()
-                prices[key] = (round, swap_price, X, Y)
+                scraped_info[key] = (round, swap_price, X, Y)
                 rounds.add(round)
 
-        if prices == last_prices:
+        # Check if API response information has not changed
+        if scraped_info == latest_scraped_info:
+            # Restarting the loop, doing the next API calls
             continue
-        last_prices = prices.copy()
+        # Keep track of the most recent API response information for future calls 
+        latest_scraped_info = scraped_info.copy()
 
+        # Check if API response information is consistent among block-rounds
         if len(rounds) != 1:
+            # Do new API response calls right away
             continue
         
         # Open the CSV file for writing
         with open("prices2.csv", "a", newline="") as f:
             # Create a CSV writer
             writer = csv.writer(f)
-            price_info_tuples = [prices[market_key] for market_key in ordered_list_of_market_keys]
+            # Get response information in uniform order
+            price_info_tuples = [scraped_info[market_key] for market_key in ordered_list_of_market_keys]
+            # Write dereferenced tuples in CSV file
             writer.writerow([x for tuple in price_info_tuples for x in tuple])
-            
-        # Sleep for 1 second
-        time.sleep(0.75)
 
 if __name__ == "__main__":
     main()
