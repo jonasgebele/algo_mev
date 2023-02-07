968829d37f66c883abc50f5f2ac80c71653f285a
Jonas Gebele <ge69cas@mytum.de>
2023-02-07 18:13:08 +0100

    Update block_data_parser.py

--- src/block_data_parser.py
+++ src/block_data_parser.py
@@ -1,67 +1,100 @@
 import requests
 from collections import defaultdict
 
+# import classes.application_call as ApplicationCall
+# import classes.payment as Payment
+
 BASE_URL = "https://algoindexer.algoexplorerapi.io/v2/blocks/"
 
-def add_payment_transaction_to_groups(transactions, application_tx_group):
+def add_transaction_data(transactions, group_collection):
     for transaction in transactions:
         if "group" in transaction:
-            group_id = transaction["group"]
-            if not (transaction in application_tx_group[group_id]):
-                application_tx_group[group_id].append(transaction)
-    return application_tx_group
+            transaction_group = transaction["group"]
+            if transaction_group in group_collection:
+                group_collection[transaction_group].append(transaction)
+    return group_collection
 
-def extract_application_call_groups(transactions: dict) -> dict:
-    transaction_groups = defaultdict(list)
+def get_swap_application_group_collection(transactions: dict) -> dict:
+    group_collection = defaultdict(list)
     for transaction in transactions:
-        if "group" in transaction:
-            group_id = transaction["group"]
-            transaction_groups[group_id].append(transaction)
-    return transaction_groups
+        if "application-transaction" in transaction:
+            application_txs = transaction["application-transaction"]
+            if is_monitored_application(application_txs["application-id"]):
+                group_collection[transaction["group"]]
+    return group_collection
 
-def get_block_data(round: int):
-    hash, transactions, timestamp = "ERROR", {}, 0
+def parse_block_data(round: int):
+    hash, timestamp, transactions = "", 0, {}
     try:
         response = requests.get(BASE_URL + str(round)).json()
-        assert round == response["round"]
-        assert response["genesis-id"] == "mainnet-v1.0"
         hash = response["genesis-hash"]
         transactions = response["transactions"]
         timestamp = response["timestamp"]
-    except requests.exceptions.RequestException as e:
+    except (requests.exceptions.RequestException, KeyError) as e:
         print(f"An request error occurred: {e}")
-    except KeyError as e:
-        print(f"An key error occurred: {e}")
-    except AssertionError as e:
-        print(f"An assertion error occurred: {e}")
     finally:
         return hash, timestamp, transactions
 
 def get_swap_interactions(block_number):
-    hash, timestamp, transactions = get_block_data(block_number)
-    application_tx_groups = extract_application_call_groups(transactions)
-    transaction_groups = add_payment_transaction_to_groups(transactions, application_tx_groups)
-    return hash, timestamp, transaction_groups
+    hash, timestamp, transactions = parse_block_data(block_number)
+    group_collection = get_swap_application_group_collection(transactions)
+    group_collection = add_transaction_data(transactions, group_collection)
+    return hash, timestamp, group_collection
 
-def get_swap_summary(transaction_group):
+def is_monitored_application(application_id):
+    # "HUMBLESWAP_ALGOUSDC" = "SVZS7Q7QMVHZONDHZJHR4564VTMEX3OQ5DSYBWKR5FJFTPZLVG3EZIWC34" = 777628254
+    # "HUMBLESWAP_ALGOgoUSD" = "LMU5MRQWB3DDSM7J3YY32OBURDL3FHEHQW7J6USTIR5J3HSCNSCDTGDTCU" = 778663643
+    # "PACT_ALGOUSDC" = "L747MOJV43QCLY4HSWVPL2A5SW62IBLA5XPTI5R4HO32WVAWOO5OBCEP3A" = 620995314
+    # "PACT_ALGOUSDT" = "KIQDDU4KRXFMUBMLQ75VN5R6RVQHNGHCEX43VSPYHGHBNW27AFNGEZ7EY4" = 667170441
+    # "TINYMAN_ALGOUSDC" = "FPOU46NBKTWUZCNMNQNXRWNW3SMPOOK4ZJIN5WSILCWP662ANJLTXVRUKA" = 552635992 # (4 txs) Here also a fee goes to Tinyman
+    # "TINYMAN_ALGOUSDT" = "54UTVEAHWMBYB4L4BNLAEJFWBUTLLERTULROEEP7774OK6FUTX4U5NX6RM" = 552635992 # (4 txs) Here also a fee goes to Tinyman
+    monitored_dex_list = [777628254, 778663643, 620995314, 667170441, 552635992]
+    return True if application_id in monitored_dex_list else False
 
-    # Parse txs-groups with 3 txs (of the common 3 DEXes)
+def get_application_call(transaction_list):
+    for transaction in transaction_list:
+        if "tx-type" in transaction:
+            if transaction["tx-type"] == "appl":
+                return transaction
+    return None
 
-    # Parse txs-groups with 4 txs (of the DEX orderbook exchange)
+def get_payment(transaction_list):
+    for transaction in transaction_list:
+        if "tx-type" in transaction:
+            if transaction["tx-type"] == "pay":
+                if "payment-transaction" in transaction:
+                    payment_transaction = transaction["payment-transaction"]
+                    if "amount" in payment_transaction:
+                        amount = payment_transaction["amount"]
+                        if amount > 2000: # Protocol-Fee for Tinyman-AMM
+                            return transaction
+    return None
 
-    # Parse txs from Algorand
+def get_swap_summary(transactions):
+    # TODO: Parse txs from AlgoFi
 
-    transaction_id = None
-    group_id = None
-    recepient = None
-    sender = None
+    application_call = get_application_call(transactions)
+    payment = get_payment(transactions)
 
-    application_id = None
+    group_id = application_call["group"]
+    originator = application_call["sender"]
+    # address of AMM
+    dex_address = payment["sender"]
+    # application_id
+    application_specs = application_call["application-transaction"]
+    application_id = application_specs["application-id"]
 
-    amount_payed = None
-    asset_id_payed = None
+    internal_txns = application_call["inner-txns"]
+    # amount_send
 
-    amount_received = None
-    asset_id_received = None
+    # asset_id_send
+    #asset_id = internal_txns["asset-id"]
 
-    return
+    # amount_received
+    # asset_id_received
+
+    # market impact on price
+    # price before swap
+    # price after swap
+
+    return "[" + group_id + "] - " + originator + "->" + "Application:" + str(application_id) + " (" + dex_address + ") "
