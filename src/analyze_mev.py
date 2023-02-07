31f03c9872192721937eb906c51c32af0453704c
Jonas Gebele <ge69cas@mytum.de>
2023-02-07 18:14:46 +0100

    Update analyze_mev.py

--- src/analyze_mev.py
+++ src/analyze_mev.py
@@ -1,10 +1,19 @@
+import argparse
 import block_data_parser
 
+def parse_block_parameter():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('block_number', type=int, help='Block we want to analyze for swaps.')
+    args = parser.parse_args()
+    return args.block_number
+
 def main():
-    block_number = 25729521
-    hash, timestamp, transaction_groups = block_data_parser.get_swap_interactions(block_number)
-    for group in transaction_groups:
-        print(transaction_groups[group])
+    block_number = parse_block_parameter()
+
+    hash, timestamp, group_collection = block_data_parser.get_swap_interactions(block_number)
+    for group in group_collection:
+        summary = block_data_parser.get_swap_summary(group_collection[group])
+        print(summary)
 
 if __name__ == "__main__":
-    main()
+    main() # Run with: python analyze_mev.py 26814040
