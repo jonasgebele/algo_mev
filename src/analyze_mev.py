4f7fe67e1d958042616f5edd86c2798a3e41285b
Jonas Gebele <ge69cas@mytum.de>
2023-02-15 18:23:44 +0100

    Update analyze_mev.py

--- src/analyze_mev.py
+++ src/analyze_mev.py
@@ -7,14 +7,29 @@ def parse_block_parameter():
     args = parser.parse_args()
     return args.block_number
 
+def estimate_dex_arbitrage_lower_bound():
+    # loop through blocks
+
+    # if price deviation on AMM is more than specified limit
+    #       Check if swap occured on AMM while deviation is more than specified limit
+    #               Check if Swap occured in favourable direction
+    #                       Calculate the extractable value
+    #                       Sum values up
+    pass
+
+def test():
+    pass
+
+def find_mev_extracters():
+    # https://plotly.com/python/network-graphs/
+    pass
+
 def main():
     block_number = parse_block_parameter()
     groups = block_parser.get_swap_interactions(block_number)
-    
-
     for group in groups:
         transactions_of_group = groups[group]
-        group_summary = block_parser.swap_summary(transactions_of_group)
+        group_summary = block_parser.extract_swap_information(transactions_of_group)
         print(group_summary)
 
 if __name__ == "__main__":
