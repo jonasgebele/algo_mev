a6a4b9e7812b5b11a41d9287fb2ce1cb595e5397
Jonas Gebele <ge69cas@mytum.de>
2023-02-20 18:48:50 +0100

    Create api_alternatives.py

---
+++ src/api_alternatives.py
@@ -0,0 +1,5 @@
+# https://api.coincap.io/v2/assets/algorand LIMIT: No-API key - 200 requests per minute, Free Tier API Key: 500 requests per minute 
+# https://docs.coincap.io/
+
+# https://www.coingecko.com/de/api/documentation - Ping api will have a limit of 1 request/sec
+# https://api.coingecko.com/api/v3/simple/price?ids=algorand&vs_currencies=usd
